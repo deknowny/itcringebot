9cb3452c767d09b6a146f677a8843465fd9e3a4f
deknowny <57015939+deknowny@users.noreply.github.com>
2021-08-15 22:01:55 +0300

    Typing and pep8 fixes

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 import os
+
+
 # TG bot token
 TG_TOKEN: str = os.environ.get('TG_TOKEN')
 # Link to TG channel (required with @)
@@ -7,4 +9,4 @@ TG_CHANNEL: str = os.environ.get('TG_CHANNEL')
 # VK group longpoll token
 VK_TOKEN: str = os.environ.get('VK_TOKEN')
 # VK API version, may don't change it
-VK_API_VERSION: int = os.environ.get('VK_API_VERSION')
+VK_API_VERSION: str = os.environ.get('VK_API_VERSION')
